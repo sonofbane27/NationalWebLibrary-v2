0cc2d6a6f33d972b1608568da8a64dd462e0f8c7
HaythamMuhamad <>
2023-04-01 16:56:23 +0000

    added files

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+BOOKS = [
+{
+  'id':'1',
+  'title':'In Search of Lost Time',
+  'author':'Marcel Proust'
+},
+{
+  'id':'2',
+  'title':'Ulysses',
+  'author':'James Joyce'
+},
+{
+  'id':'3',
+  'title':'The Great Gatsby',
+  'author':'F. Scott Fitzgerald'
+},
+{
+  'id':'4',
+  'title':'Moby Dick',
+  'author':'Herman Melville'
+}  
+]
+
 
 @app.route("/")
 def hello_world():
   return render_template('home.html')
+
+@app.route("/results")
+def search_results():
+  return render_template('search.html', books=BOOKS)
 
 
 if __name__ == '__main__':
